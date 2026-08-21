d24c2d48632598251825f38bbfb086f1330f4950
Thiago <duartethiago017@gmail.com>
2026-08-21 13:12:49 -0300

    Dashboard-Style

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -5,12 +5,8 @@ function getDate() {
     document.getElementById('welcome-message').textContent = formattedDate;
 }
 
-function logOutt() {
-    document.getElementById('logout-btn').addEventListener('click', () => {
-        // Esses comando removem os dados do usuário do localStorage ao fazer logout
-        localStorage.removeItem('auth.users(email)');
-        localStorage.removeItem('auth.users(id)');
-        // redireciona o usuário para a página de login (index.html)
-        window.location.href = 'index.html';
+document.getElementById('logout-btn').addEventListener('click', () => {
+      localStorage.removeItem('herois_user');
+      localStorage.removeItem('herois_token');
+      window.location.href = 'index.html';
     });
-}
